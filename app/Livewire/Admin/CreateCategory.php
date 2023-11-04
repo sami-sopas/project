923be46b461d381a01c3839005a965081f293615
sami-sopas <ramirezcuenca@outlook.es>
2023-11-03 23:00:12 -0600

    Updating categories !!! with modals and moar

--- app/Livewire/Admin/CreateCategory.php
+++ app/Livewire/Admin/CreateCategory.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 
 use Illuminate\Support\Str;
 use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Storage;
 
 class CreateCategory extends Component
 {
@@ -16,6 +17,8 @@ class CreateCategory extends Component
 
     public $categories; //Cargar aqui todas las categorias
 
+    public $category; //Para el edit, tener la categoria actual
+
     //Input values, para imprimirlos en blade es con {{$createForm['name']}}
     public $createForm = [
         'name' => null,
@@ -23,7 +26,19 @@ class CreateCategory extends Component
         'image' => null,
     ];
 
-    //Reglas de validacion
+    //Inputs para cuando se este editando
+    public $editForm = [
+        'open' => false, //Para controlar el modal
+        'name' => null,
+        'slug' => null,
+        'image' => null,
+    ];
+
+    //Almacenar la posible imagen a reemplazar
+    public $editImage;
+
+
+    //Reglas de validacion al crear
     protected $rules = [
         'createForm.name' => 'required',
         'createForm.slug' => 'required|unique:categories,slug',
@@ -35,6 +50,9 @@ class CreateCategory extends Component
         'createForm.name' => 'nombre',
         'createForm.slug' => 'slug',
         'createForm.image' => 'imagen',
+        'editForm.name' => 'nombre',
+        'editForm.slug' => 'slug',
+        'editImage' => 'imagen',
     ];
 
     public function mount()
@@ -54,8 +72,14 @@ class CreateCategory extends Component
     //Esta funcion escucha cada que cambia el valor de name, para asi cambiar el slug
     public function updatedCreateFormName($value)
     {
-        //Actualizamos el slug
+        //Actualizamos el slug al momento de crear
         $this->createForm['slug'] = Str::slug($value);
+    }
+
+    //Actualizar slug pero ahora para editar
+    public function updatedEditFormName($value)
+    {
+        $this->editForm['slug'] = Str::slug($value);
     }
 
     public function save()
@@ -84,6 +108,63 @@ class CreateCategory extends Component
         //Emitir evento para el mensaje de Categoria agregada
         $this->dispatch('saved');
 
+
+    }
+
+    //Mostrar modal
+    public function edit(Category $category)
+    {
+        //Resetear mensajes de error que hayan salido en otras partes
+        $this->resetValidation();
+
+        //Resetear imagen si es que se subio una nueva
+        $this->reset('editImage');
+
+        $this->category = $category;
+
+        //Abrir modal
+        $this->editForm['open'] = true;
+
+        //Obtener los campos
+        $this->editForm['name'] = $category->name;
+        $this->editForm['slug'] = $category->name;
+        $this->editForm['image'] = $category->image;
+    
+    }
+
+    //Actualizar en base a los campos de editForm
+    public function update()
+    {
+        $rules = [
+            'editForm.name' => 'required',
+            'editForm.slug' => 'required|unique:categories,slug,' . $this->category->id,
+        ];
+
+        //Se almaceno una nueva imagen?
+        if($this->editImage){
+            //Agregamos una validacion neuva para esa imagen
+            $rules['editImage'] =  'image|max:1024';
+        }
+
+        $this->validate($rules);
+
+        //Se subio una nueva imagen? Eliminamos a la anterior
+        if($this->editImage) {
+            //Le pasamos la url de la img
+            Storage::delete([$this->editForm['image']]); 
+
+            //Reemplazarmos por la nueva imagen (la cual es la que esta en editImage), se guarda en disco
+            $this->editForm['image'] = $this->editImage->store('categories');
+        }   
+
+        //Actualizamos con los nuevos datos de los inputs
+        $this->category->update($this->editForm);
+
+        //Reseteamos los inputs
+        $this->reset(['editForm','editImage']);
+
+        //Actualizamos las categorias
+        $this->getCategories();
 
     }
 
--- resources/views/livewire/admin/create-category.blade.php
+++ resources/views/livewire/admin/create-category.blade.php
@@ -91,9 +91,10 @@
                             </td>
                             <td class="py-2 ">
                                 <div class="flex divide-x divide-gray-300 font-semibold">
-                                    <a class="pr-2 text-blue-600 cursor-pointer">Editar</a>
+                                    {{-- Aqui emitimos un evento para abrir un modal --}}
+                                    <a class="pr-2 text-blue-600 cursor-pointer" wire:click="edit('{{$category->slug}}')">Editar</a>
 
-                                    {{-- Emitir evento a script de sweetAlert, le enviamos el Id de la categoria --}}
+                                    {{-- Emitir evento al controlador, le pasamos el id de la categoria --}}
                                     <a class="pl-2 text-red-600 cursor-pointer" 
                                     wire:click="deleteCategory({{ $category->id }})">Eliminar</a>
                                 </div>
@@ -105,5 +106,71 @@
         </x-slot>
 
     </x-action-section>
+
+    {{-- MODAL --}}
+    <x-dialog-modal wire:model="editForm.open">
+
+        <x-slot name="title">
+            Editar categoria
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="space-y-3">
+                <div>
+
+                    <x-label>
+                        Nombre
+                    </x-label>
+
+                    <x-input wire:model="editForm.name" type="text" class="w-full mt-1" />
+
+                    <x-input-error for="editForm.name" />
+                </div>
+
+                <div>
+
+                    <x-label>
+                        Slug
+                    </x-label>
+
+                    <x-input disabled type="text" wire:model="editForm.slug" class="w-full bg-gray-100 mt-1" />
+
+                    <x-input-error for="editForm.slug" />
+                </div>
+
+                <div>
+
+                    <x-label>
+                        Imagen
+                    </x-label>
+
+                    <input type="file" class="mt-1" name="" 
+                    wire:model="editImage"
+                        accept="image/png, image/jpeg" id="{{ $rand }}" />
+
+                    <x-input-error for="editImage" />
+                </div>
+
+                {{--Mostrar imagen actual--}}
+                <div>
+                    @if ($editImage)
+                        {{-- Mostrar imagen que subio--}}     {{-- Accediendo a la carpeta temporal donde se guarda la imagen--}}
+                        <img class="w-full h-64 object-cover object-center" src="{{$editImage->temporaryUrl()}}" alt="">    
+                    @else
+                        {{-- Mostrar imagen antes de actualizar (la original) --}}
+                        <img class="w-full h-64 object-cover object-center" src="{{ Storage::url($editForm['image']) }}" alt="">    
+                    @endif
+                </div>
+            </div>
+        </x-slot>
+
+        <x-slot name="footer">
+                        {{-- Ejecutar la funcion update del controlador --}}
+            <x-button wire:click="update">
+                Actualizar
+            </x-button>
+        </x-slot>
+
+    </x-dialog-modal>
 
 </div>
