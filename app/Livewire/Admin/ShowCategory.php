977d574a5614419bfe602452ca1c903f23e42250
sami-sopas <ramirezcuenca@outlook.es>
2023-11-04 22:11:01 -0600

    CRUD for subcategories

--- app/Livewire/Admin/ShowCategory.php
+++ app/Livewire/Admin/ShowCategory.php
@@ -2,16 +2,122 @@
 
 namespace App\Livewire\Admin;
 
-use App\Models\Category;
 use Livewire\Component;
+use App\Models\Category;
+use App\Models\Subcategory;
+use Illuminate\Support\Str;
 
 class ShowCategory extends Component
 {
-    public $category;
+    public $category, $subcategories, $subcategory;
+
+    //Para los inputs del crear
+    public $createForm = [
+        'name' => null,
+        'slug' => null,
+        //Saber si esta subcategoria tendra color o talla
+        'color' => false,
+        'size' => false,
+    ];
+
+    //Para los inputs del modal
+    public $editForm = [
+        'open' => false, //control modal
+        'name' => null,
+        'slug' => null,
+        //Saber si esta subcategoria tendra color o talla
+        'color' => false,
+        'size' => false,
+    ];
+
+    //Reglas de validacion al crear
+    protected $rules = [
+        'createForm.name' => 'required',
+        'createForm.slug' => 'required|unique:subcategories,slug',
+        'createForm.color' => 'required',
+        'createForm.size' => 'required',
+    ];
+
+    //Mensajes de error
+    protected $validationAttributes =[
+        'createForm.name' => 'nombre',
+        'createForm.slug' => 'slug',
+    ];
+
 
     public function mount(Category $category)
     {
         $this->category = $category;
+        $this->getSubcategories();
+    }
+
+    public function getSubcategories(){
+        $this->subcategories = Subcategory::where('category_id',$this->category->id)->get();
+    }
+
+    public function updatedCreateFormName($value)
+    {
+        //Actualizamos el slug al momento de crear
+        $this->createForm['slug'] = Str::slug($value);
+    }
+
+    public function updatedEditFormName($value)
+    {
+        //Actualizamos el slug al momento de crear
+        $this->editForm['slug'] = Str::slug($value);
+    }
+
+    //Guardar subcategoria
+    public function save()
+    {
+        $this->validate();
+
+        //Crearemos la subcategoria a partir de su relacion con la categoria
+        $this->category->subcategories()->create($this->createForm);
+
+        $this->reset('createForm');
+
+        $this->getSubcategories();
+    }
+
+    //Modal para editar
+    public function edit(Subcategory $subcategory)
+    {
+        //Resetear mensajes de validaciones
+        $this->resetValidation();
+
+        //Guardar la subcategoria seleccionada
+        $this->subcategory = $subcategory;
+
+        //Abrir modal
+        $this->editForm['open'] = true;
+
+        //LLenarlo con la informacion adecuada
+        $this->editForm['name'] = $subcategory->name;
+        $this->editForm['slug'] = $subcategory->slug;
+        $this->editForm['color'] = $subcategory->color;
+        $this->editForm['size'] = $subcategory->size;
+    }
+
+    public function update()
+    {
+        $this->validate([
+            'editForm.name' => 'required',
+            'editForm.slug' => 'required|unique:subcategories,slug,' . $this->subcategory->id,
+            'editForm.color' => 'required',
+            'editForm.size' => 'required',
+        ]);
+
+        $this->subcategory->update($this->editForm);
+
+        $this->getSubcategories();
+        $this->reset('editForm');
+    }
+
+    public function deleteSubcategory(Subcategory $subcategory)
+    {
+        $subcategory->delete();
+        $this->getSubcategories();
     }
 
     public function render()
--- resources/views/livewire/admin/show-category.blade.php
+++ resources/views/livewire/admin/show-category.blade.php
@@ -1,3 +1,226 @@
-<div>
-    {{-- Success is as dangerous as failure. --}}
+<div class="container py-12">
+    {{-- Formulario para subcategoria --}}
+    <x-form-section submit="save" class="mb-6"> {{-- Ejecuta el metodo save del controlador Createsubcategory --}}
+        <x-slot name="title">
+            Nueva subcategoria
+        </x-slot>
+
+        <x-slot name="description">
+            LLene los campos inge
+        </x-slot>
+
+        <x-slot name="form">
+
+            <div class="col-span-6 sm:col-span-4">
+
+                <x-label>
+                    Nombre
+                </x-label>
+
+                <x-input wire:model="createForm.name" type="text" class="w-full mt-1" />
+
+                <x-input-error for="createForm.name" />
+            </div>
+
+            <div class="col-span-6 sm:col-span-4">
+
+                <x-label>
+                    Slug
+                </x-label>
+
+                <x-input disabled type="text" wire:model="createForm.slug" class="w-full bg-gray-100 mt-1" />
+
+                <x-input-error for="createForm.slug" />
+            </div>
+
+            <div class="col-span-6 sm:col-span-4">
+                <div class="flext">
+                    <p>¿Esta subcategoria necesita color?</p>
+
+                    <div class="ml-auto">
+                        <label for="">
+                            <input type="radio" name="color" value="1" wire:model.defer="createForm.color">
+                            Si
+                        </label>
+
+                        <label for="">
+                            <input type="radio" name="color" value="0" wire:model.defer="createForm.color">
+                            No
+                        </label>
+                    </div>
+
+                </div>
+                <x-input-error for="createForm.color" />
+            </div>
+
+
+            <div class="col-span-6 sm:col-span-4">
+                <div class="flext items-center">
+                    <p>¿Esta subcategoria necesita talla?</p>
+
+                    <div class="ml-auto">
+                        <label for="">
+                            <input type="radio" name="size" value="1" wire:model.defer="createForm.size">
+                            Si
+                        </label>
+
+                        <label for="">
+                            <input type="radio" name="size" value="0" wire:model.defer="createForm.size">
+                            No
+                        </label>
+                    </div>
+
+                </div>
+                <x-input-error for="createForm.size" />
+            </div>
+
+
+
+        </x-slot>
+
+        <x-slot name="actions">
+
+            <x-action-message class="mr-3" on="saved"> {{-- Escucha al evento saved --}}
+                Categoria agregada
+            </x-action-message>
+
+            <x-button>
+                Agregar
+            </x-button>
+        </x-slot>
+
+    </x-form-section>
+
+    {{-- Listado y botones para editar/eliminar --}}
+    <x-action-section>
+
+        <x-slot name="title">
+            Subategorias
+        </x-slot>
+
+        <x-slot name="description">
+            Aqui tiene las subcategorias pertenecientes a <strong>{{ $category->name }}</strong>
+        </x-slot>
+
+        <x-slot name="content">
+            <table>
+                <thead class="border-b border-gray-400">
+                    <tr class="text-left">
+                        <th class="py-2 w-full">Nombre</th>
+                        <th class="py-2">Accion</th>
+                    </tr>
+                </thead>
+
+                <tbody class="divide-y divide-gray-300">
+                    @foreach ($subcategories as $subcategory)
+                        <tr>
+                            <td class="py-2">
+                                <span class="capitalize">
+                                    {{ $subcategory->name }}
+                                </span>
+                            </td>
+                            <td class="py-2 ">
+                                <div class="flex divide-x divide-gray-300 font-semibold">
+                                    {{-- Aqui emitimos un evento para abrir un modal --}}
+                                    <a class="pr-2 text-blue-600 cursor-pointer"
+                                        wire:click="edit('{{ $subcategory->id }}')">Editar</a>
+
+                                    {{-- Emitir evento al controlador, le pasamos el id de la categoria --}}
+                                    <a class="pl-2 text-red-600 cursor-pointer"
+                                        wire:click="deleteSubcategory({{ $subcategory->id }})">Eliminar</a>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </x-slot>
+
+    </x-action-section>
+
+    {{-- MODAL --}}
+    <x-dialog-modal wire:model="editForm.open">
+
+        <x-slot name="title">
+            Editar Subcategoria
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="space-y-3">
+                <div>
+
+                    <x-label>
+                        Nombre
+                    </x-label>
+
+                    <x-input wire:model="editForm.name" type="text" class="w-full mt-1" />
+
+                    <x-input-error for="editForm.name" />
+                </div>
+
+                <div>
+
+                    <x-label>
+                        Slug
+                    </x-label>
+
+                    <x-input disabled type="text" wire:model="editForm.slug" class="w-full bg-gray-100 mt-1" />
+
+                    <x-input-error for="editForm.slug" />
+                </div>
+
+                <div>
+                    <div class="flext">
+                        <p>¿Esta subcategoria necesita color?</p>
+    
+                        <div class="ml-auto">
+                            <label for="">
+                                <input type="radio" name="color" value="1" wire:model.defer="editForm.color">
+                                Si
+                            </label>
+    
+                            <label for="">
+                                <input type="radio" name="color" value="0" wire:model.defer="editForm.color">
+                                No
+                            </label>
+                        </div>
+    
+                    </div>
+                    <x-input-error for="editForm.color" />
+                </div>
+    
+    
+                <div>
+                    <div class="flext items-center">
+                        <p>¿Esta subcategoria necesita talla?</p>
+    
+                        <div class="ml-auto">
+                            <label for="">
+                                <input type="radio" name="size" value="1" wire:model.defer="editForm.size">
+                                Si
+                            </label>
+    
+                            <label for="">
+                                <input type="radio" name="size" value="0" wire:model.defer="editForm.size">
+                                No
+                            </label>
+                        </div>
+    
+                    </div>
+                    <x-input-error for="editForm.size" />
+                </div>
+
+                
+
+            </div>
+        </x-slot>
+
+        <x-slot name="footer">
+            {{-- Ejecutar la funcion update del controlador --}}
+            <x-button wire:click="update">
+                Actualizar
+            </x-button>
+        </x-slot>
+
+    </x-dialog-modal>
 </div>
